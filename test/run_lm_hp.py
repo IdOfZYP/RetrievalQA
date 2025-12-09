aea16afda91124b918839174325d766ea1365e61
zhanyuanpeng <yp@example.com>
2025-12-09 20:04:26 +0800

    nlp任务不使用torch.compile

--- test/run_lm_hp.py
+++ test/run_lm_hp.py
@@ -202,7 +202,7 @@ def load_model(args):
         attn_implementation="sdpa"  # 显式指定使用 PyTorch 原生加速关注力
     )
 
-    model = torch.compile(model, dynamic=True)
+    # model = torch.compile(model, dynamic=True)
 
     # 创建pipeline
     pipe = pipeline(
@@ -414,3 +414,5 @@ if __name__ == '__main__':
 # 100%|██████████| 200/200 [03:36<00:00,  1.08s/it]
 # model = torch.compile(model, dynamic=True)，attn_implementation="sdpa"
 # 100%|██████████| 200/200 [03:37<00:00,  1.09s/it]
+# attn_implementation="sdpa"
+# 100%|██████████| 200/200 [03:36<00:00,  1.08s/it]
